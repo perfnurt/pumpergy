6afe1174e44f9e4f973976541374011c564067ed
perfnurt <perfnurt@gmail.com>
2026-04-04 00:14:59 +0200

    Dynamic date range

--- app.py
+++ app.py
@@ -108,9 +108,9 @@ with st.sidebar:
     # Date range
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input("Start Date", value=datetime(2026, 2, 1))
+        start_date = st.date_input("Start Date", value=datetime.now() - timedelta(days=60))
     with col2:
-        end_date = st.date_input("End Date", value=datetime(2026, 3, 31))
+        end_date = st.date_input("End Date", value=datetime.now())
     
     st.divider()
     st.header("🦠 Legionella Schedule")
